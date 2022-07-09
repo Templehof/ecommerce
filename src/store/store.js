413637d015ebf3c73b35b99ed1861d6ad77ae747
Ivan Riumkin <ivri0001@gmail.com>
2022-07-09 10:55:53 +0200

    added react-perssist for cart data

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,24 @@
 /* import { compose, applyMiddleware } from "redux"; */
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
 const middlewares = [logger];
 
-/*const composedEnhancers = compose(applyMiddleware(...middlewares)); */
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: middlewares,
 });
+
+export const persistor = persistStore(store);
